12e55e6e49353a204b3e06c4171fc34db13a49d0
Kimginam97 <rlarlska79@naver.com>
2023-08-09 15:57:23 +0900

    modify: r3f-perf 오른쪽 바닥에 배치

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link ,Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
 
 import ThreeScene from './components/three/ThreeScene'
 import DashBoard from './pages/DashBoard'
@@ -8,11 +8,11 @@ export default function App() {
   return (
     <>
       <Router basename={import.meta.env.VITE_PUBLIC_URL}>
-      <Link to="/dashboard">dashboard</Link>
+        <Link to="/dashboard">dashboard</Link>
         <Routes>
           <Route path="/" element={<ThreeScene />} />
-            <Route path="/dashboard" element={<DashBoard />} />
-            <Route path="/template" element={<Template />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/template" element={<Template />} />
         </Routes>
       </Router>
     </>
