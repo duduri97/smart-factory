c8e6cba637a0ad285afbffba8e77804a78fcc52b
Kimginam <65409092+Kimginam97@users.noreply.github.com>
2023-08-09 15:52:31 +0900

    Dev (#8)
    * modify: env 설정변경
    
    * link 테스트
    
    ---------
    
    Co-authored-by: duduri97 <140989618+duduri97@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link ,Routes, Route } from 'react-router-dom'
 
 import ThreeScene from './components/three/ThreeScene'
 import DashBoard from './pages/DashBoard'
@@ -9,10 +9,11 @@ export default function App() {
   return (
     <>
       <Router basename={import.meta.env.VITE_PUBLIC_URL}>
+        <Link to="/dashboard">dashboard</Link>
         <Routes>
-            <Route path="/" element={<ThreeScene />} />
-            <Route path="/dashboard" element={<DashBoard />} />
-            <Route path="/template" element={<Template />} />
+          <Route path="/" element={<ThreeScene />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/template" element={<Template />} />
         </Routes>
       </Router>
     </>
