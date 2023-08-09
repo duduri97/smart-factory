5621dfad96c9b16990dd2ae29bc2b5840071e7cd
duduri97 <140989618+duduri97@users.noreply.github.com>
2023-08-09 15:27:55 +0900

    modify: env 설정변경 (#7)
    Co-authored-by: Kimginam97 <rlarlska79@naver.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import ThreeScene from './components/three/ThreeScene'
 import DashBoard from './pages/DashBoard'
 import Template from './pages/Template'
 
 export default function App() {
+  console.log(import.meta.env.VITE_PUBLIC_URL)
   return (
     <>
-      <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+      <Router basename={import.meta.env.VITE_PUBLIC_URL}>
         <Routes>
-          <Route path="/" element={<ThreeScene />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="/template" element={<Template />}  />
+            <Route path="/" element={<ThreeScene />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+            <Route path="/template" element={<Template />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
