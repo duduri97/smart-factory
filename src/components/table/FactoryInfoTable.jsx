2a45d76f56ab8b06f6cd66106bf71d19704db0db
Kimginam <65409092+Kimginam97@users.noreply.github.com>
2023-08-24 18:24:26 +0900

    Dev (#29)
    * modify: env 설정변경
    
    * link 테스트
    
    * modify: r3f-perf 오른쪽 바닥에 배치
    
    * remove: console.log
    
    * add: Router 경로 추가
    
    * modify : Loader
    
    * add
    
    * add: react-prosidebar
    - 설치
    npm install @mui/icons-material @mui/material @emotion/styled @emotion/react react-pro-sidebar@1.0.0
    
    * add: 화면 레이아웃 구성
    
    * package.json merge 충돌 해결
    
    * 화면 레이아웃 설정 home, dashboard
    
    * sidebar 링크 추가
    
    * table 추가
    
    * 가동상태 기능 추가
    
    * 오류수정
    
    * 소모전력, 압력 추가
    
    * 추가요청사항
    - 사이드바 logo img 수정
    - 가동상태 버튼클릭 x
    - 실시간 정보 추가
    
    * 사이드바 링크 추가
    
    * table 커스텀마이징
    
    ---------
    
    Co-authored-by: duduri97 <140989618+duduri97@users.noreply.github.com>
    Co-authored-by: jeongchanyong <chanyong.jeong@kakao.com>
    Co-authored-by: musiz <103082970+JeongChanyong@users.noreply.github.com>

--- src/components/table/FactoryInfoTable.jsx
+++ src/components/table/FactoryInfoTable.jsx
@@ -5,6 +5,9 @@ const style = {
     fontSize: 'large',
     color: '#dbcfe1',
     padding: '0.1rem 0 0 0.7rem'
+  },
+  td: {
+    color:'white'
   }
 }
 
@@ -15,27 +18,27 @@ const FactoryInfoTable = () => {
       <Table className="table-dark table-bordered border-secondary" bordered responsive hover>
         <thead>
           <tr>
-            <th>순번</th>
-            <th>Process</th>
-            <th colSpan={2}>In</th>
-            <th colSpan={2}>Out</th>
+            <th style={style.td}>순번</th>
+            <th style={style.td}>Process</th>
+            <th style={style.td} colSpan={2}>In</th>
+            <th style={style.td} colSpan={2}>Out</th>
           </tr>
           <tr>
-            <th colSpan={2}></th>
-            <th>Date</th>
-            <th>Time</th>
-            <th>Date</th>
-            <th>Time</th>
+            <th style={style.td} colSpan={2}></th>
+            <th style={style.td}>Date</th>
+            <th style={style.td}>Time</th>
+            <th style={style.td}>Date</th>
+            <th style={style.td}>Time</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>IQC</td>
-            <td>08/09</td>
-            <td>09:10</td>
-            <td>08/09</td>
-            <td>14:20</td>
+            <td style={style.td}>1</td>
+            <td style={style.td}>IQC</td>
+            <td style={style.td}>08/09</td>
+            <td style={style.td}>09:10</td>
+            <td style={style.td}>08/09</td>
+            <td style={style.td}>14:20</td>
           </tr>
         </tbody>
       </Table>
--- src/components/table/StateHistoryTable.jsx
+++ src/components/table/StateHistoryTable.jsx
@@ -1,32 +1,37 @@
 import { Table } from 'react-bootstrap'
 
+import '../table/custom-table.scss'
+
 const style = {
   title: {
     fontSize: 'large',
     color: '#dbcfe1',
     padding: '0.1rem 0 0 0.7rem'
   },
+  td: {
+    color:'white'
+  }
 }
 
 const StateHistoryTable = () => {
   return (
     <>
       <div style={style.title}>상태이력조회</div>
-      <Table className="table-dark table-bordered border-secondary" bordered responsive hover>
+      <Table className="table-dark table-bordered border-secondary"  bordered responsive hover>
         <thead>
           <tr>
-            <th>이상 감지 일자</th>
-            <th>내역</th>
+            <th style={style.td}>이상 감지 일자</th>
+            <th style={style.td} >내역</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>23-05-01</td>
-            <td>전력이상</td>
+            <td style={style.td}>23-05-01</td>
+            <td style={style.td}>전력이상</td>
           </tr>
           <tr>
-            <td>23-08-02</td>
-            <td>전력이상</td>
+            <td style={style.td}>23-08-02</td>
+            <td style={style.td}>전력이상</td>
           </tr>
         </tbody>
       </Table>
